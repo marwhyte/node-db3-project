81e2c54351cf99dc631a2706d3af8789396576c4
Marco Whyte <mwhyte@umass.edu>
2020-03-19 15:15:18 -0500

    fixed find steps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -18,16 +18,18 @@ function findById(id) {
     .first();
 }
 function findSteps(id) {
-  return db("schemes")
-    .join(
-      "steps",
+  return db("steps")
+    .join("schemes", "schemes.id", "steps.scheme_id")
+    .select(
+      "steps.id",
       "schemes.scheme_name",
       "steps.step_number",
       "steps.instructions"
     )
-    .where("shemes.id", id)
+    .where("schemes.id", id)
     .orderBy("steps.step_number");
 }
+
 function add(item) {
   return db("schemes").insert(item);
 }
